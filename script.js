0e7ecbd605ab6a74b5d0b7bac6f1ecea89079243
PRABHAKAR9107 <02panjiyara@gmail.com>
2019-10-23 15:43:57 -0400

    data load

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://hplussport.com/api/products?qty=2&order=name');
+request.open("GET", "https://hplussport.com/api/products");
 
 request.onload = function() {
-	var response = request.response;
-	var parsedData = JSON.parse(response);
-	console.log(parsedData);
-	var name = parsedData[0].name;
-	
-	var products = document.createElement('li');
-	products.innerHTML = parsedData[0].name;
-	document.body.appendChild(products);
+  var response = request.response;
+  var parsedData = JSON.parse(response);
+  console.log(parsedData);
+
+  for (item in parsedData) {
+    var name = parsedData[item].name;
+    var products = document.createElement("li");
+    products.innerHTML = name;
+    document.body.appendChild(products);
+
+    var imagePic = parsedData[item].image;
+    var images = document.createElement("img");
+    images.setAttribute("src", imagePic);
+    document.body.appendChild(images);
+  }
 };
 
 request.send();
